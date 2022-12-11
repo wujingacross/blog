97766907755f461bfcd34cef543a54f5d4ef2e21
wujing23 <wujing23@58.com>
2022-12-11 15:51:54 +0800

    feat(blog): mdx样式2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "components/Layout";
-import utilStyles from "styles/utils.module.css";
 
 export default function Home() {
   return (
